256d9321c0bd132af7e0d6d3142a25c8480975b5
alexthayn <alexthayn>
2018-10-17 18:57:32 -0600

    First commit

--- HW08/Views/MainWindow.xaml.cs
+++ HW08/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HW08
+namespace HW08.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -22,7 +22,7 @@ namespace HW08
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();  
         }
     }
 }
